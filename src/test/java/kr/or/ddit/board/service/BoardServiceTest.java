d61ae0669b216377ba96ad91d0429516cc268e03
YC <mazexit@gmail.com>
2019-06-07 08:49:38 +0900

    commit test

--- src/test/java/kr/or/ddit/board/service/BoardServiceTest.java
+++ src/test/java/kr/or/ddit/board/service/BoardServiceTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.List;
 
+import kr.or.ddit.board.model.ArticleVO;
 import kr.or.ddit.board.model.BoardVO;
 
 import org.junit.Before;
@@ -33,6 +34,19 @@ public class BoardServiceTest {
 		/***Then***/
 		assertEquals("취미게시판", boardVo.get(2).getBoard_name());
 		
+	}
+	
+	@Test
+	public void selectAllArticleTest(){
+		/***Given***/
+		String article_board = "b2";
+
+		/***When***/
+		List<ArticleVO> articleList = boardService.selectAllArticle(article_board);
+
+		/***Then***/
+		assertEquals(1, articleList.size());
+
 	}
 
 }
